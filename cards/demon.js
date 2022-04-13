b6d5671399ef6a0ff60c28ec511573ca82d733e1
White_JL <jason.li@mdland.com>
2022-04-13 17:57:24 -0400

    Basic player ID implemented except 'drunk'

--- cards/demon.js
+++
@@ -1,7 +0,0 @@
-class Demon extends Card {
-
-}
-
-const demons = [imp];
-
-var imp = new Card('Imp', '小恶魔', 'Minions', 'Evil');
--- cards/outsiders.js
+++
@@ -1,8 +0,0 @@
-import Card from card.js
-
-var Outsiders = [Butler, Drunk, Recluse, Saint]
-
-var Butler = new Card('Butler', '管家', 'Outsider','Good')
-var Drunk = new Card('Drunk', '酒鬼', 'Outsider','Good')
-var Recluse = new Card('Recluse', '隐士', 'Outsider','Good')
-var Saint = new Card('Saint','圣徒', 'Outsider','Good')
